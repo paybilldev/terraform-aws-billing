227c026ccaa255f56e2fbf7aa71a11af75b2acd1
Emmanuel Bartile <ebartile@gmail.com>
2026-01-10 22:47:19 +0300

    first commit

---
+++ main.tf
@@ -0,0 +1,27 @@
+module "budgets" {
+  source  = "cloudposse/budgets/aws"
+  version = "0.4.1"
+
+
+  budgets = var.budgets
+
+  notifications_enabled = var.slack_notifications_enabled
+  encryption_enabled    = var.encryption_enabled
+  kms_master_key_id     = var.budgets_kms_master_key
+
+  slack_webhook_url = var.slack_webhook_url
+  slack_channel     = var.slack_channel
+  slack_username    = var.slack_username
+
+  context = module.this.context
+}
+
+resource "aws_sns_topic_subscription" "this" {
+  for_each = var.billing_alerts_sns_subscribers
+
+  topic_arn              = var.slack_notifications_enabled ? module.budgets.sns_topic_arn : var.sns_topic_arn
+  protocol               = each.value.protocol
+  endpoint               = each.value.endpoint
+  endpoint_auto_confirms = each.value.endpoint_auto_confirms
+  raw_message_delivery   = each.value.raw_message_delivery
+}
